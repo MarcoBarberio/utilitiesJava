d30bbb995c0b7d6d9df2250424bea126087391e9
mbarb <mbarb@LAPTOP-14LJQUMV>
2023-10-24 11:24:06 +0200

    SimpleExceptionHandler

---
+++ src/SimpleExceptionHandler.java
@@ -0,0 +1,33 @@
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.TimeUnit;
+
+public class SimpleExceptionHandler {
+
+	public static void awaitTermination(ExecutorService threadPool) {
+		try {
+			threadPool.awaitTermination(Long.MAX_VALUE, TimeUnit.MILLISECONDS);
+		} catch (InterruptedException e) {
+			System.out.println("threadPool waiting termination");
+		}
+	}
+
+	public static void closeFile(InputStream stream) {
+		try {
+			stream.close();
+		} catch (IOException e) {
+			System.out.println("Error closing file");
+		}
+	}
+
+	public static void execute(ExecutorService threadPool, Runnable task) {
+		try {
+			threadPool.execute(task);
+		} catch (RejectedExecutionException e) {
+			System.out.println("Rejected task error");
+		}
+	}
+}
